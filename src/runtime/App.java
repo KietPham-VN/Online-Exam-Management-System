d90bf0c639c788635b60899cdef14e0ed36f4dc4
ggbb0711 <ggbotcute07@gmail.com>
2024-10-22 14:28:08 +0700

    Test the new object in main

--- src/runtime/App.java
+++ src/runtime/App.java
@@ -1,53 +1,25 @@
 package runtime;
 
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
-import com.microsoft.sqlserver.jdbc.SQLServerException;
-import java.sql.Connection;
-import java.sql.ResultSet;
+import db.database;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class App {
 
   @SuppressWarnings("deprecation")
-  public static void main(String[] args) throws SQLException {
+  public static void main(String[] args) {
     String server = "urjellyfish.mssql.somee.com";
     String user = "urjellyfish_SQLLogin_1";
     String password = "5medi3d4w6";
     String db = "urjellyfish";
     int port = 1433;
-    SQLServerDataSource ds = new SQLServerDataSource();
-    ds.setUser(user);
-    ds.setPassword(password);
-    ds.setDatabaseName(db);
-    ds.setServerName(server);
-    ds.setPortNumber(port);
-    ds.setEncrypt(false);
-    ds.setTrustServerCertificate(true);
-
-    try {
-      Connection conn = ds.getConnection();
-      Statement stmt = null;
-      try {
-        stmt = conn.createStatement();
-      } catch (SQLException ex) {
-        Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-      }
-
-      // Truy vấn SELECT
-      String sql = "SELECT UserID, Username, Email, Role, CreatedAt FROM tbl_Users";
-      ResultSet rs = stmt.executeQuery(sql);
-
-      // Duyệt qua kết quả và in ra
-      while (rs.next()) {
-        System.out.println("UserID: " + rs.getInt("UserID") + ", Username: " + rs.getString("Username") + 
-                           ", Email: " + rs.getString("Email") + ", Role: " + rs.getString("Role") +
-                           ", CreatedAt: " + rs.getTimestamp("CreatedAt"));
-      }
-    } catch (SQLServerException e) {
-      System.out.println(e);
+    database ds = new database(server,user,password,db,port);
+    
+    try{
+        ds.connect();
     }
+    catch(Exception ex){
+        System.out.println(ex);
+    }
+ 
   }
 }
