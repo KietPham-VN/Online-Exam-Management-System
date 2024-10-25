808a61c548c207fa139b9b7ea4c997bb0499f2c5
ggbb0711 <ggbotcute07@gmail.com>
2024-10-25 19:38:37 +0700

    Make sure that the choice is in the correct range

---
+++ src/ui/AdminMenu.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+/**
+ *
+ * @author NGHIA
+ */
+public class AdminMenu {
+    public void Print(){
+        while(true){
+            
+        }
+    }
+}
