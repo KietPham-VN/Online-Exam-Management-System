fc41d5619df228d20a69aaeddd29f50db302bc3d
Phạm Anh Kiệt <phamanhkiet.dev@gmail.com>
2024-10-30 02:30:31 +0700

    add methods

--- src/data/Choice.java
+++ src/data/Choice.java
@@ -2,49 +2,49 @@ package data;
 
 public class Choice {
 
-  private int choiceID;
-  private int questionID;
-  private String choiceText;
-  private boolean isCorrect;
+    private int choiceID;
+    private int questionID;
+    private String choiceText;
+    private boolean isCorrect;
 
-  // Constructor
-  public Choice(int choiceID, int questionID, String choiceText, boolean isCorrect) {
-    this.choiceID = choiceID;
-    this.questionID = questionID;
-    this.choiceText = choiceText;
-    this.isCorrect = isCorrect;
-  }
+    // Constructor
+    public Choice(int choiceID, int questionID, String choiceText, boolean isCorrect) {
+        this.choiceID = choiceID;
+        this.questionID = questionID;
+        this.choiceText = choiceText;
+        this.isCorrect = isCorrect;
+    }
 
-  // Getters and Setters
-  public int getChoiceID() {
-    return choiceID;
-  }
+    // Getters and Setters
+    public int getChoiceID() {
+        return choiceID;
+    }
 
-  public void setChoiceID(int choiceID) {
-    this.choiceID = choiceID;
-  }
+    public void setChoiceID(int choiceID) {
+        this.choiceID = choiceID;
+    }
 
-  public int getQuestionID() {
-    return questionID;
-  }
+    public int getQuestionID() {
+        return questionID;
+    }
 
-  public void setQuestionID(int questionID) {
-    this.questionID = questionID;
-  }
+    public void setQuestionID(int questionID) {
+        this.questionID = questionID;
+    }
 
-  public String getChoiceText() {
-    return choiceText;
-  }
+    public String getChoiceText() {
+        return choiceText;
+    }
 
-  public void setChoiceText(String choiceText) {
-    this.choiceText = choiceText;
-  }
+    public void setChoiceText(String choiceText) {
+        this.choiceText = choiceText;
+    }
 
-  public boolean isCorrect() {
-    return isCorrect;
-  }
+    public boolean isCorrect() {
+        return isCorrect;
+    }
 
-  public void setCorrect(boolean isCorrect) {
-    this.isCorrect = isCorrect;
-  }
+    public void setCorrect(boolean isCorrect) {
+        this.isCorrect = isCorrect;
+    }
 }
